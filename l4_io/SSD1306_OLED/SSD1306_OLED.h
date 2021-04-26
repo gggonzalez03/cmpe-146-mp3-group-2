c4e01ea1ae38d64e50d5140d6e6780877b9399d5
ginggonzalez <ginggonzalez3@gmail.com>
2021-04-25 19:43:55 -0700

    cleanup: Do some cleanup. Remove test code from main

--- l4_io/SSD1306_OLED/SSD1306_OLED.h
+++ l4_io/SSD1306_OLED/SSD1306_OLED.h
@@ -3,73 +3,12 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-bool SSD1306_OLED_initialize(void);
+bool SSD1306__initialize(void);
 
 void SSD1306__write(uint8_t command, uint8_t data);
-SSD1306__command_write(uint8_t command, uint8_t *data, uint8_t size);
+void SSD1306__command_write(uint8_t command, uint8_t *data, uint8_t size);
 void SSD1306__data_write(uint8_t data);
 void SSD1306__horizontalscroll();
+void SSD1306__delay_ms(uint32_t ms);
 
-#if 0
-typedef struct {
-  char header[10];
-  char tag[50];
-} vs1053b__mp3_song;
-
-/**
- * Initialize the decoder
- * @return whether initialization is successful or not
- **/
-bool vs1053b__mp3_decoder_initialize(void);
-
-/**
- * @return whether the decoder can take more data or not
- **/
-bool vs1053b__mp3_decoder_needs_data(void);
-
-/**
- * Start the mp3 audio data transfer
- **/
-void vs1053b__mp3_decoder_start();
-
-/**
- * Transfer mp3 audio data
- **/
-bool vs1053b__mp3_decoder_play_byte(uint8_t byte);
-
-/**
- * End the mp3 audio data transfer
- **/
-void vs1053b__mp3_decoder_end();
-
-/**
- * Get the status of the decoderr (Status register value)
- **/
-uint16_t vs1053b__get_status(void);
-
-/**
- * Write data to a register on the vs1053b chip
- **/
-void vs1053b__sci_write(uint8_t address, uint16_t data);
-
-/**
- * Read data from a registerr on the vs1053b chip
- **/
-uint16_t vs1053b__sci_read(uint8_t address);
-
-/**
- * Soft reset the vs1053b chip
- **/
-void vs1053b__soft_reset(void);
-
-/**
- * Hard reset the vs1053b chip
- **/
-void vs1053b__reset(void);
-
-/**
- * Do a sine test on the vs1053b chip
- **/
-void vs1053b__sine_test(uint8_t n, uint32_t duration_in_ms);
-
-#endif
+void SSD1306__alphabet_test(void);
