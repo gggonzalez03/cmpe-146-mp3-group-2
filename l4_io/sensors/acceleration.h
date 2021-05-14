7680b5adad97d7b3fa557ced74ce75cbd2c197b1
ginggonzalez <ginggonzalez3@gmail.com>
2021-05-14 00:16:04 -0700

    feat: output acceleration in m/s^2 and add functions to enable interrupts
    We can now detect orientation changes

--- l4_io/sensors/acceleration.h
+++ l4_io/sensors/acceleration.h
@@ -3,13 +3,33 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-// Data for x-axis, y-axis, and z-axis
-typedef struct {
-  int16_t x, y, z;
-} acceleration__axis_data_s;
+typedef enum {
+  ACC__SRC_DRDY = 0,
+  ACC__SRC_FF_MT = 4,
+  ACC__SRC_PULSE = 8,
+  ACC__SRC_LNDPRT = 16,
+  ACC__SRC_TRANS = 32,
+  ACC__SRC_ASLP = 64
+} acceleration__interrupt_source_e;
 
-/// Enables acceleration sensor data update and returns true if we successfully detected the sensor
+typedef enum {
+  ACC__ORRIENTATION_UP = 0,
+  ACC__ORRIENTATION_DOWN,
+  ACC__ORRIENTATION_RIGHT,
+  ACC__ORRIENTATION_LEFT,
+  ACC__ORRIENTATION_UNKNOWN
+} acceleration__orientation_e;
+
 bool acceleration__init(void);
 
-/// @returns data of all 3-axis of the sensor
-acceleration__axis_data_s acceleration__get_data(void);
+float acceleration__get_acceleration_x(void);
+
+float acceleration__get_acceleration_y(void);
+
+float acceleration__get_acceleration_z(void);
+
+void acceleration__enable_orientation_interrupts(void);
+
+acceleration__orientation_e acceleration__get_orientation(void);
+
+acceleration__interrupt_source_e acceleration__get_interrupt_source(void);
--- l4_io/sensors/sources/acceleration.c
+++ l4_io/sensors/sources/acceleration.c
@@ -1,15 +1,40 @@
 #include "acceleration.h"
-
 #include "i2c.h"
 
 static const i2c_e acceleration__sensor_bus = I2C__2;
 static const uint8_t acceleration__address = 0x38;
+static const uint8_t acceleration__memory_axis_x = 0x01;
+static const uint8_t acceleration__memory_axis_y = 0x03;
+static const uint8_t acceleration__memory_axis_z = 0x05;
+static const uint8_t acceleration__memory_control = 0x2A;
+static const uint8_t acceleration__memory_who_am_i = 0x0D;
 
-typedef enum {
-  acceleration__memory_axis_base = 0x01,
-  acceleration__memory_control = 0x2A,
-  acceleration__memory_who_am_i = 0x0D,
-} acceleration__memory_e;
+static void acceleration__activate(void) {
+  uint8_t ctrl_reg_1 = 0x2A;
+  uint8_t ctrl_reg_1_value;
+  uint8_t activate = 0x01;
+
+  i2c__read_slave_data(acceleration__sensor_bus, acceleration__address, ctrl_reg_1, &ctrl_reg_1_value, 1);
+  i2c__write_single(acceleration__sensor_bus, acceleration__address, ctrl_reg_1, ctrl_reg_1_value | activate);
+}
+
+static void acceleration__deactivate(void) {
+  uint8_t ctrl_reg_1 = 0x2A;
+  uint8_t ctrl_reg_1_value;
+  uint8_t deactivate = 0x01;
+
+  i2c__read_slave_data(acceleration__sensor_bus, acceleration__address, ctrl_reg_1, &ctrl_reg_1_value, 1);
+  i2c__write_single(acceleration__sensor_bus, acceleration__address, ctrl_reg_1, ctrl_reg_1_value & ~(deactivate));
+}
+
+static uint8_t acceleration__get_orientation_status_reg(void) {
+  uint8_t orientation_reg = 0x10;
+  uint8_t orientation_reg_value;
+
+  i2c__read_slave_data(acceleration__sensor_bus, acceleration__address, orientation_reg, &orientation_reg_value, 1);
+
+  return orientation_reg_value;
+}
 
 bool acceleration__init(void) {
   const uint8_t active_mode_with_100Hz = (1 << 0) | (3 << 3);
@@ -23,22 +48,89 @@ bool acceleration__init(void) {
   return (who_am_i_expected_value == who_am_i_actual_value);
 }
 
-acceleration__axis_data_s acceleration__get_data(void) {
-  acceleration__axis_data_s axis_data = {0};
+float acceleration__get_acceleration_x(void) {
+  uint8_t axis_x[2] = {0};
 
-  uint8_t axis_raw_data[6] = {0};
-  i2c__read_slave_data(acceleration__sensor_bus, acceleration__address, acceleration__memory_axis_base, axis_raw_data,
-                       sizeof(axis_raw_data));
+  int16_t raw_x = 0;
+  float x = 0.0f;
 
-  // First byte is MSB then LSB
-  axis_data.x = ((uint16_t)axis_raw_data[0] << 8) | axis_raw_data[1];
-  axis_data.y = ((uint16_t)axis_raw_data[2] << 8) | axis_raw_data[3];
-  axis_data.z = ((uint16_t)axis_raw_data[4] << 8) | axis_raw_data[5];
+  i2c__read_slave_data(acceleration__sensor_bus, acceleration__address, acceleration__memory_axis_x, axis_x,
+                       sizeof(axis_x));
 
-  // Data is at upper 12-bits, so shift the result down by 4
-  axis_data.x /= 16;
-  axis_data.y /= 16;
-  axis_data.z /= 16;
+  raw_x = (((int16_t)axis_x[0] << 8) | axis_x[1]);
+  x = (float)raw_x / 1024.0f / 16.0f * 9.8f;
 
-  return axis_data;
+  return x;
+}
+
+float acceleration__get_acceleration_y(void) {
+  uint8_t axis_y[2] = {0};
+
+  int16_t raw_y = 0;
+  float y = 0.0f;
+
+  i2c__read_slave_data(acceleration__sensor_bus, acceleration__address, acceleration__memory_axis_y, axis_y,
+                       sizeof(axis_y));
+
+  raw_y = (((int16_t)axis_y[0] << 8) | axis_y[1]);
+  y = (float)raw_y / 1024.0f / 16.0f * 9.8f;
+
+  return y;
+}
+
+float acceleration__get_acceleration_z(void) {
+  uint8_t axis_z[2] = {0};
+
+  int16_t raw_z = 0;
+  float z = 0.0f;
+
+  i2c__read_slave_data(acceleration__sensor_bus, acceleration__address, acceleration__memory_axis_z, axis_z,
+                       sizeof(axis_z));
+
+  raw_z = (((int16_t)axis_z[0] << 8) | (axis_z[1]));
+  z = (float)raw_z / 1024.0f / 16.0f * 9.8f;
+
+  return z;
+}
+
+void acceleration__enable_orientation_interrupts(void) {
+  uint8_t ctrl_reg_4 = 0x2D;
+  uint8_t orientation_cfg_reg = 0x11;
+  uint8_t orientation_interrupt_en = 1 << 4;
+  uint8_t orientation_detection_en = 1 << 6;
+
+  acceleration__deactivate();
+
+  i2c__write_single(acceleration__sensor_bus, acceleration__address, ctrl_reg_4, orientation_interrupt_en);
+  i2c__write_single(acceleration__sensor_bus, acceleration__address, orientation_cfg_reg, orientation_detection_en);
+
+  acceleration__activate();
+}
+
+acceleration__interrupt_source_e acceleration__get_interrupt_source(void) {
+  uint8_t int_source_reg = 0x0C;
+  uint8_t int_source;
+
+  acceleration__deactivate();
+
+  i2c__read_slave_data(acceleration__sensor_bus, acceleration__address, int_source_reg, &int_source, 1);
+
+  acceleration__activate();
+
+  return int_source;
+}
+
+acceleration__orientation_e acceleration__get_orientation(void) {
+  uint8_t orientation_status_reg_val = acceleration__get_orientation_status_reg();
+
+  uint8_t landscape_or_portrait = orientation_status_reg_val & (3UL << 2);
+  uint8_t back_or_front = orientation_status_reg_val & (1UL << 0);
+
+  acceleration__orientation_e orientation = landscape_or_portrait >> 1;
+
+  if (back_or_front == 1) {
+    return ACC__ORRIENTATION_UNKNOWN;
+  }
+
+  return orientation;
 }
